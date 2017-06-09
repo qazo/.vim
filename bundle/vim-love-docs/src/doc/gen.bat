0b671aa31d383e0fc5ea0031d41e976c2df011c2
Kwezilomso Mhaga <kwezimhaga@live.com>
2017-06-09 13:30:37 +0200

    deleted plugin directories

--- bundle/vim-love-docs/src/doc/gen.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-pushd %~dp0
-if not exist ..\..\doc mkdir ..\..\doc
-love . > ..\..\doc\love.txt
-vim -c "helptags ..\..\doc\" -c "qa!"
-
-rd /q /s love-api
-popd
--- bundle/vim-love-docs/src/doc/gen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd "$(dirname "$0")"
-set -e
-mkdir -p ../../doc/
-love .> ../../doc/love.txt
-vim -c "helptags ../../doc/" -c "qa!"
-rm -rf love-api
-
