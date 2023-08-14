d4f104378e52f95538c25f8e3fd89d22edab0e1d
Kwezilomso Mhaga <kwezimhaga@live.com>
2023-08-15 01:07:49 +0200

    change keymap for format command

--- lua/qazo/lspconfig.lua
+++ lua/qazo/lspconfig.lua
@@ -21,6 +21,6 @@ function module.on_attach(client, bufnr)
 	vim.keymap.set('n', '<Leader>rn', vim.lsp.buf.rename, bufopts)
 	vim.keymap.set('n', '<Leader>.', vim.lsp.buf.code_action, bufopts)
 	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-	vim.keymap.set('n', '<Leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+	vim.keymap.set('n', '<Leader>F', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
 return module
