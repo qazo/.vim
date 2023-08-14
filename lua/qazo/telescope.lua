d3d7635be8e19fe130df3f97d6b7aef09946e85f
Kwezilomso Mhaga <kwezimhaga@live.com>
2023-08-15 01:33:53 +0200

    remap telescope keys

--- lua/qazo/telescope.lua
+++ lua/qazo/telescope.lua
@@ -3,11 +3,9 @@ local builtin = require('telescope.builtin')
 local module = {}
 
 module.setup_keys = function()
-	vim.keymap.set('n', '<Leader>pf', builtin.find_files, {})
+	vim.keymap.set('n', '<Leader>ff', builtin.find_files, {})
 	vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-	vim.keymap.set('n', '<Leader>ps', function()
-		builtin.grep_string({ search = vim.fn.input("Grep > ") })
-	end)
+	vim.keymap.set('n', '<Leader>fs', builtin.live_grep, {})
 end
 
 module.setup = function()
