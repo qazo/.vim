31cb82a0f7cbdcc5c7f3e6f4ea4e4975bea0d80f
Kwezilomso Mhaga <kwezimhaga@live.com>
2024-04-21 17:52:35 +0200

    add search with preview in telescope keybind

--- lua/qazo/telescope.lua
+++ lua/qazo/telescope.lua
@@ -6,6 +6,12 @@ module.setup_keys = function()
 	vim.keymap.set('n', '<Leader>ff', builtin.find_files, {})
 	vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 	vim.keymap.set('n', '<Leader>fs', builtin.live_grep, {})
+	vim.keymap.set('n', '<Leader>/', function()
+		builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+			winblend = 10,
+			previewer = false,
+		})
+	end, {})
 end
 
 module.setup = function()
